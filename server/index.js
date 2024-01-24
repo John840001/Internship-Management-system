2c445f70f29ef1d91dd7777bb440fccfab5e1c2b
Ra-1007 <thegamer4268@gmail.com>
2024-01-24 21:34:35 +0530

    now all good

--- server/index.js
+++ server/index.js
@@ -2,7 +2,6 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const path = require("path");
 const connection = require("./db");
 const userRoutes = require("./routes/users");
 const authRoutes = require("./routes/auth");
@@ -17,9 +16,3 @@ app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-
-app.use(express.static(path.join(__dirname, "../client/build")));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
-});

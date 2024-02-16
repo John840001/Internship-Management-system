595dbccde96bfb549fa587bd7ed77448d37e02ad
Ra-1007 <thegamer4268@gmail.com>
2024-02-16 19:08:15 +0530

    ui

--- client/src/components/navBar/adminNavbar.js
+++ client/src/components/navBar/adminNavbar.js
@@ -2,7 +2,7 @@ import { Link, NavLink } from "react-router-dom";
 
 const AdminNavBar = () => {
   return (
-    <nav className="bg-gray-800">
+    <nav className="bg-red-500">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
--- client/src/pages/Admin/index.js
+++ client/src/pages/Admin/index.js
@@ -1,6 +1,10 @@
-
+import CardBarChart from "../../components/cards/CardBarChart";
+import CardSettings from "../../components/cards/CardSettings";
+import CardStats from "../../components/cards/CardStats";
 import AdminNavBar from "../../components/navBar/adminNavbar";
 import React, { useState, useEffect } from "react";
+import AdminNavBar1 from "../../components/navBar/adminNavbar1";
+import CardLineChart from "../../components/cards/CardLineChart";
 
 const dummyAdminData = {
     name: "Admin User",
@@ -25,14 +29,27 @@ const AdminHome = () => {
     return (
         <div>
           <AdminNavBar/>
-              <div className="p-8 bg-blue-200">
+              <div className="p-8 bg-sky-700">
               
               <h1 className="text-4xl font-bold">Admin Home</h1>
+              
+              <AdminNavBar1></AdminNavBar1>
+              
+              
+              <div className="flex flex-wrap">
+        <div className="w-full xl:w-8/12 mb-12 xl:mb-0 px-4">
+          <CardLineChart />
+        </div>
+        <div className="w-full xl:w-4/12 px-4">
+          <CardBarChart />
+        </div>
+      </div>
+              
               {loading ? (
                   <p>Loading...</p>
               ) : (
                   <div className="space-y-8">
-                      <div className="bg-white p-4 rounded shadow-lg space-y-4">
+                      <div className="bg-indigo-500 p-4 rounded shadow-lg space-y-4">
                           <h2 className="text-2xl font-bold">Profile</h2>
                           <div className="space-y-2">
                               <p className="font-semibold">Name:</p>
